f9fb2f3bb1fdb4ec7ff121fb88176dad97636fc1
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2025-04-10 12:30:45 -0400

    eaae

--- app/src/components/SignIn.vue
+++ app/src/components/SignIn.vue
@@ -1,5 +1,8 @@
 <template>
-  <div><script src="https://apis.google.com/js/platform.js" async defer></script></div>
+  <div>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id" content="{process.env.GOAUTH_KEY}" />
+  </div>
 </template>
 
 <script setup lang="ts"></script>

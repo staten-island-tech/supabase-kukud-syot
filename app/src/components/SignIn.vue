1f24b24070f93c1e1fd99046f9196a0865181796
raymondd71 <raymondd71@nycstudents.net>
2025-04-10 12:30:32 -0400

    Added button useless

--- app/src/components/SignIn.vue
+++ app/src/components/SignIn.vue
@@ -1,5 +1,7 @@
 <template>
-  <div><script src="https://apis.google.com/js/platform.js" async defer></script></div>
+  <div>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+  </div>
 </template>
 
 <script setup lang="ts"></script>

52d51962071bd5d20935d9d76a7cc84dc719aa80
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2025-04-20 22:52:49 -0400

    e

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import SignIn from './components/SignIn.vue'
 </script>
 
 <template>
   <div>
+    <SignIn />
     <h1 class="text-center text-5xl">LOGO</h1>
     <div class="min-h-screen flex flex-col pt-16">
       <div class="flex-1 p-4">
--- app/src/components/SignIn.vue
+++ app/src/components/SignIn.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <meta name="google-signin-client_id" content="{process.env.GOAUTH_KEY}" />
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+declare global {
+  interface Window {
+    onSignIn: (googleUser: any) => void
+  }
+}
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile()
+  console.log('ID: ' + profile.getId())
+  console.log('Name: ' + profile.getName())
+  console.log('Image URL: ' + profile.getImageUrl())
+  console.log('Email: ' + profile.getEmail())
+}
+</script>
 
 <style scoped></style>

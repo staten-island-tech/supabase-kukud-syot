515aaf2ce9b73b8245024cddf82175f7af28ad64
raymondd71 <raymondd71@nycstudents.net>
2025-04-23 12:28:54 -0400

    ada

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -17,7 +17,15 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Accounts/MainSignin.vue'),
     },
-    
+    {
+      path: '/signup',
+      name: 'signup',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Accounts/MainSignup.vue'),
+    },
+
   ],
 })
 

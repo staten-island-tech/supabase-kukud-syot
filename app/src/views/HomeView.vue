12118af182e42bd7a9f01acf6d038eaac3370990
raymondd71 <raymondd71@nycstudents.net>
2025-04-23 12:32:24 -0400

    fix something in sign up.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <RouterLink to="/signin" class="flex justify-center"> Sign In</RouterLink
     ><!--  when this is clicked the page will bring you to
     page with /signin and /sign in will be the stuff in mainsignin.vue -->
+    <RouterLink to="/signup" class="flex justify-center"> Sign Up</RouterLink>
   </div>
 </template>
 

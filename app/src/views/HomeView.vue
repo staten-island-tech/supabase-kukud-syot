3874659e98993a413682f4dc1a477452cd466c98
raymondd71 <raymondd71@nycstudents.net>
2025-04-28 12:32:49 -0400

    kllk

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
-    <RouterLink to="/signin" class="flex justify-center"> Sign In</RouterLink
+  <div class="border border-gray-300 rounded-lg w-50 text-center">
+    <RouterLink to="/signin" class="flex justify-center" style="font-size: 30px">
+      Sign In</RouterLink
     ><!--  when this is clicked the page will bring you to
     page with /signin and /sign in will be the stuff in mainsignin.vue -->
-    <RouterLink to="/signup" class="flex justify-center"> Sign Up</RouterLink>
+    <RouterLink to="/signup" class="flex justify-center" style="font-size: 30px">
+      Sign Up</RouterLink
+    >
   </div>
 </template>
 

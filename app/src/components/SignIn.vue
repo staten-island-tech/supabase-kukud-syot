4b75cc0ec0103f0aeaf4227a226f39bacf93925d
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2025-05-09 12:01:40 -0400

    Revert "Merge branch 'main' into DesignPages"
    This reverts commit a0401e14d13c81a8b0942426ff8dd51f7e6dba7e, reversing
    changes made to b65b13f2461130609ded218ef371084d88b7340e.

--- app/src/components/SignIn.vue
+++ app/src/components/SignIn.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <div id="appleid-signin" data-color="black" data-border="true" data-type="sign in"></div>
+    <div id="g_id_signin"></div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { GoAuthKey, supabase } from '../../GetKeys'
+import { GoAuthKey, supabase } from '../GetKeys'
 
 async function handleCredentialResponse(response) {
   const { data, error } = await supabase.auth.signInWithIdToken({
-    provider: 'apple',
+    provider: 'google',
     token: response.credential,
   })
 }
--- app/src/views/Accounts/MainSignin.vue
+++ app/src/views/Accounts/MainSignin.vue
@@ -2,17 +2,15 @@
   <div>
     <MSignIn />
     <button class="text-xl">Sign In</button>
-    <GSignIn class="flex justify-center" />
-    <ASignIn />
+    <SignIn class="flex justify-center" />
     <!-- when flex is added, the width of the google login button changes. IDK why -->
     <RouterLink to="/" class="flex justify-center">Return</RouterLink>
   </div>
 </template>
 
 <script setup>
-import GSignIn from '../../components/account providers/SignIn.vue'
-import ASignIn from '../../components/account providers/ASignIn.vue'
-import MSignIn from '../../components/account providers/MSignIn.vue'
+import SignIn from '../../components/SignIn.vue'
+import MSignIn from '../../components/MSignIn.vue'
 </script>
 
 <style scoped></style>

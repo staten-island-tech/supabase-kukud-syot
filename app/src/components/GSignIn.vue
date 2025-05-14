f47316eec366cd67ff74aaa1edcf1b2070a7be2a
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2025-05-14 00:46:49 -0400

    ae

--- app/src/components/GSignIn.vue
+++ app/src/components/GSignIn.vue
@@ -9,10 +9,37 @@ import { onMounted } from 'vue'
 import { GoAuthKey, supabase } from '../GetKeys'
 
 async function handleCredentialResponse(response) {
+  const googleDisplayName = response?.credential?.displayName
   const { data, error } = await supabase.auth.signInWithIdToken({
     provider: 'google',
     token: response.credential,
   })
+
+  if (data) {
+    //works
+    if (googleDisplayName) {
+      addData(googleDisplayName)
+    }
+  }
+
+  if (error) {
+    //fail
+    console.error('Error signing in:', error.message)
+  }
+}
+
+const addData = async (display_name: string) => {
+  if (!display_name) return
+
+  try {
+    //push to public.users
+    const { data, error } = await supabase.from('users').insert([{ display_name: display_name }])
+
+    if (error) throw error
+    console.log('display name inserted to supabase:', data)
+  } catch (error) {
+    console.error('error adding display name:', error.message)
+  }
 }
 
 onMounted(() => {

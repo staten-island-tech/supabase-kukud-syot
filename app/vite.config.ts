6f16d9a525dccd632b7cccf2659ceb6b1d2af3e4
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2025-05-17 22:24:59 -0400

    tae

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,17 +3,32 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Pages from 'vite-plugin-pages'
+import generateSitemap from 'vite-plugin-pages-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    Pages({
+      onRoutesGenerated: (routes) => {
+        generateSitemap({
+          routes,
+          hostname: 'https://crocodillo.org',
+          dest: 'public',
+        })
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
-    allowedHosts: ['devserver-main--shrekscavern.netlify.app'],
+    allowedHosts: ['devserver-main--crocodillo.netlify.app'],
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',

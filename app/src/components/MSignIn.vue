e23557cb4b0bd4e0e1e1bfeb11f4a8f8f2e4950c
raymondd71 <raymondd71@nycstudents.net>
2025-05-21 12:28:33 -0400

    CSS Change
    made the sign in stuff centered and created it so that i liked how it looked
    
    future ref: change user to email;

--- app/src/components/MSignIn.vue
+++ app/src/components/MSignIn.vue
@@ -1,7 +1,11 @@
 <!-- manual sign in -->
 <template>
-  <div>
-    <h1 class="flex justify-center">User: <input type="text" placeholder=" Enter username" /></h1>
+  <div
+    class="flex flex-col items-center justify-center w-128 h-64 border l-50px border-black bg-white border-rounded-lg shadow-lg rounded-md"
+  >
+    <h1 class="flex item-center justify-center">
+      Username: <input type="text" placeholder=" Enter username" />
+    </h1>
     <h1 class="flex justify-center">
       Password:
       <input type="text" placeholder=" Enter password" />

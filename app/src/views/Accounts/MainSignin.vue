aaa4d175a90e7eb9af8015ed9da80b5a9a27ccc6
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2025-05-29 12:25:31 -0400

    Update MainSignin.vue

--- app/src/views/Accounts/MainSignin.vue
+++ app/src/views/Accounts/MainSignin.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <MSignIn />
+    <MSignIn class="flex justify-center" />
     <!-- when flex is added, the width of the google login button changes. IDK why -->
   </div>
 </template>
 
 <script setup>
-import GSignIn from '../../components/Manage Accounts/GSignIn.vue'
 import MSignIn from '../../components/Manage Accounts/MSignIn.vue'
 </script>
 

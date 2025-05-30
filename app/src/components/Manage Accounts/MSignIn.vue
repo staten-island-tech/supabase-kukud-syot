e25759d7344b2294b7e4c2097dfbe6c643653097
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2025-05-30 12:49:24 -0400

    fvvfvf

--- app/src/components/Manage Accounts/MSignIn.vue
+++ app/src/components/Manage Accounts/MSignIn.vue
@@ -1,17 +1,19 @@
 <!-- manual sign in -->
 <template>
-  <div
-    class="flex flex-col items-center justify-center w-128 h-64 border l-50px border-black bg-white border-rounded-lg shadow-lg rounded-md"
-  >
-    <h1 class="flex item-center justify-center">
-      Username: <input type="text" placeholder=" Enter username" class="w-[14ch]" />
-    </h1>
-    <h1 class="flex justify-center">
-      Password: <input type="text" placeholder=" Enter password" class="w-[14ch]" />
-    </h1>
-    <br />
-    <GSignIn class="flex justify-center" />
-    <RouterLink to="/" class="flex justify-center">Return</RouterLink>
+  <div class="min-h-screen flex items-center justify-center">
+    <div
+      class="flex flex-col items-center justify-center w-128 h-64 border l-50px border-black bg-white border-rounded-lg shadow-lg rounded-md"
+    >
+      <h1 class="flex item-center justify-center">
+        Username: <input type="text" placeholder=" Enter username" class="w-[14ch]" />
+      </h1>
+      <h1 class="flex justify-center">
+        Password: <input type="text" placeholder=" Enter password" class="w-[14ch]" />
+      </h1>
+      <br />
+      <GSignIn class="flex justify-center" />
+      <RouterLink to="/" class="flex justify-center">Return</RouterLink>
+    </div>
   </div>
 </template>
 
--- app/src/components/Manage Accounts/SignUp.vue
+++ app/src/components/Manage Accounts/SignUp.vue
@@ -1,28 +1,30 @@
 <template>
-  <div
-    class="flex flex-col items-center justify-center w-128 h-64 border l-50px border-black bg-white border-rounded-lg shadow-lg rounded-md"
-  >
-    <h1>
-      Email: <input v-model="email" type="text" placeholder="Enter an email" class="w-[12ch]" />
-    </h1>
-    <h1>
-      Password:
-      <input v-model="password" type="password" placeholder="Enter password" class="w-[13ch]" />
-    </h1>
-    <button
-      @click="signUp"
-      class="flex flex-col justify-center mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+  <div class="min-h-screen flex items-center justify-center">
+    <div
+      class="flex flex-col items-center justify-center w-128 h-64 border l-50px border-black bg-white border-rounded-lg shadow-lg rounded-md"
     >
-      Sign Up
-    </button>
+      <h1>
+        Email: <input v-model="email" type="text" placeholder="Enter an email" class="w-[12ch]" />
+      </h1>
+      <h1>
+        Password:
+        <input v-model="password" type="password" placeholder="Enter password" class="w-[13ch]" />
+      </h1>
+      <button
+        @click="signUp"
+        class="flex flex-col justify-center mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Sign Up
+      </button>
 
-    <!--  <RouterLink
+      <!--  <RouterLink
       to="/signin"
       class="flex justify-center mt-2 border-t-2 border-gray-300 pt-2 text-black hover:text-blue-800"
     >
       Have an account? Sign In</RouterLink
     > -->
-    <RouterLink to="/" class="flex justify-center">Return</RouterLink>
+      <RouterLink to="/" class="flex justify-center">Return</RouterLink>
+    </div>
   </div>
 </template>
 

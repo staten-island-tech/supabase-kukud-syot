acdc4b4d128c52990b08c5702ba23a01b00aeba0
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2025-05-30 12:44:22 -0400

    fbfb

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,21 +1,23 @@
 <template>
-  <div
-    class="flex w-128 h-64 items-center justify-center border l-50px border-black bg-white border-rounded-lg shadow-lg rounded-md"
-  >
-    <RouterLink
-      to="/signin"
-      class="border border-gray-500 rounded-lg w-50 text-center flex justify-center"
-      style="font-size: 30px"
+  <div class="min-h-screen flex items-center justify-center">
+    <div
+      class="flex items-center justify-center w-128 h-64 border-black border bg-white shadow-lg rounded-md"
     >
-      Sign In</RouterLink
-    ><!--  when this is clicked the page will bring you to page with /signin and /sign in will be the stuff in mainsignin.vue -->
-    <RouterLink
-      to="/signup"
-      class="border border-gray-500 rounded-lg w-50 text-center flex justify-center"
-      style="font-size: 30px"
-    >
-      Sign Up</RouterLink
-    >
+      <RouterLink
+        to="/signin"
+        class="border border-gray-500 rounded-lg w-50 text-center bg-white"
+        style="font-size: 30px"
+      >
+        Sign In</RouterLink
+      ><!--  when this is clicked the page will bring you to page with /signin and /sign in will be the stuff in mainsignin.vue -->
+      <RouterLink
+        to="/signup"
+        class="border border-gray-500 rounded-lg w-50 text-center bg-white"
+        style="font-size: 30px"
+      >
+        Sign Up</RouterLink
+      >
+    </div>
   </div>
 </template>
 

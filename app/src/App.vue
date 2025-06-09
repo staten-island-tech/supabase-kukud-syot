756c5e43f6628a655eed49d1331bb0d6f17aa08e
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2025-06-09 03:25:18 -0400

    silly

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,7 +4,10 @@ import { RouterView } from 'vue-router'
 
 <template>
   <div>
-    <div class=""><h1 class="text-center text-5xl">LOGO</h1></div>
+    <div class="flex items-center justify-center">
+      <img src="/favicon.ico" alt="icon" class="flex h-12 w-auto" />
+      <h1 class="flex text-center text-5xl">LOGO</h1>
+    </div>
     <div class="min-h-screen flex flex-col pt-16">
       <div class="flex-1 p-4">
         <RouterView />

d1c4f1450506e10c3d6de55ce08db904e0183705
raymondd71 <raymondd71@nycstudents.net>
2025-06-12 11:40:41 -0400

    Update HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <RouterLink to="/login">login</RouterLink>
+    <h1 class="text-5xl">Welcome NameHERE</h1>
+    <br />
+    <hr />
+    <br />
+    <RouterLink to="/settings" class=""> Settings </RouterLink>
   </div>
 </template>
 
-<script setup lang="ts">
-import LoginView from './LoginView.vue'
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
